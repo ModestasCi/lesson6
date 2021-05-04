44b229dc380a6ed8893c6f29602008379711feda
ModestasCi <modestas.ciunka@gmail.com>
2021-05-04 21:32:38 +0300

    Add project files.

---
+++ lesson6/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6
+{
+    class Car
+    {
+        private Dictionary<int, char> MyDictionary { get; set; }
+        public string Color { get; private set; }
+        public int RatuSkaicius { get; private set; }
+
+        public Car()
+        {
+            MyDictionary = new Dictionary<int, char>();
+        }
+
+        public char GetRecordFromDictionary(int key)
+        {
+            if (MyDictionary.ContainsKey(key))
+            {
+                return MyDictionary[key];
+            }
+
+            return 'a';
+        }
+
+        public void AddRecordToDictionary(int param1, char param2)
+        {
+            if (MyDictionary.ContainsKey(param1))
+            {
+                MyDictionary[param1] = param2;
+            }
+            else
+            {
+                MyDictionary.Add(param1, param2);
+            }
+        }
+
+        public void SetColor(string color)
+        {
+            if (color == "red" || color == "blue" || color == "green")
+            {
+                this.Color = color;
+            }
+        }
+
+        public void SetRatuSkaicius(int ratuSkaicius)
+        {
+            if (ratuSkaicius >= 4)
+            {
+                RatuSkaicius = ratuSkaicius;
+            }
+        }
+    }
+}
---
+++ lesson6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6
+{
+    class Program
+    {
+        static string name;
+
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.SetRatuSkaicius(4);
+            myCar.SetRatuSkaicius(6);
+
+            name = "Modestas";
+
+            //myCar.SetColor("blue");
+            //myCar.SetColor("black");
+            //string color = myCar.Color;
+
+        }
+    }
+}
